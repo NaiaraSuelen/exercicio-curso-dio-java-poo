ebc66bd60f9df0e31921f04864e60455e01b04fe
NaiaraSuelen <na_suelen25@hotmail.com>
2022-01-27 14:00:23 -0300

    Mentoria e Curso

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import br.com.dio.desafio.dominio.Curso;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso("curso Java", "descrição curso Java", 8);
+
+        System.out.println(curso1);
+    }
+}
